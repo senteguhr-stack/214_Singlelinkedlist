eebfbb0c3d0b3365a9411f450560821467e07fb3
senteguhr <senteguhr@gmail.com>
2026-04-21 12:22:43 +0700

    struktur

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -28,4 +28,13 @@ public:
         cout << "\nMasukkan NIM Mahasiswa: ";
         cin >> nim;
 
-  
+        Node *Nodebaru = new Node();
+        Nodebaru->noMhs = nim;
+
+        if (START == NULL || START->noMhs >= Nodebaru->noMhs)
+        {
+            if (START != NULL && nim == START->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak di ijinkan\n";
+                return;
+            }
