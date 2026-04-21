2de0fa28d55fecc51e868eb942e487a0db524146
senteguhr <senteguhr@gmail.com>
2026-04-21 12:25:50 +0700

    struktur

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -118,3 +118,32 @@ public:
 };
 
 int main()
+{
+    LinkedList mhs;
+    int nim;
+    char ch;
+
+    do
+    {
+        cout << "\nMenu: \n";
+        cout << "1. Menambah data ke dalam list\n";
+        cout << "2. Menghapus Data dari dalam list\n";
+        cout << "3. Menampilkan semua data di dalam list\n";
+        cout << "4. Mencari data dalam list" << endl;
+        cout << "5. Keluar\n";
+
+        cout << "Masukkan pilihan (1-5): ";
+        cin >> ch;
+
+        switch (ch)
+        {
+        case '1':
+            mhs.addNode();
+            break;
+
+        case '2':
+            if (mhs.listEmpty())
+            {
+                cout << "\nList Kosong" << endl;
+                break;
+            }
