aaf1d344df1872c7a525b96cf0419ec7485fa4bf
senteguhr <senteguhr@gmail.com>
2026-04-21 12:24:02 +0700

    struktur

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -57,3 +57,25 @@ public:
             previous = current;
             current = current->next;
         }
+
+        Nodebaru->next = current;
+        previous->next = Nodebaru;
+    }
+
+    bool listEmpty()
+    {
+        return START == NULL;
+    }
+
+    bool search(int nim, Node **previous, Node **current)
+    {
+        *previous = START;
+        *current = START;
+
+        while (*current != NULL && nim != (*current)->noMhs)
+        {
+            *previous = *current;
+            *current = (*current)->next;
+        }
+        return (*current != NULL);
+   
