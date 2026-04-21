0d0e2cf46849d179a7bdd21dbac3d3cdb4b0aab6
senteguhr <senteguhr@gmail.com>
2026-04-21 12:24:41 +0700

    struktur

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -78,4 +78,24 @@ public:
             *current = (*current)->next;
         }
         return (*current != NULL);
+    }
+
+    bool delNode(int nim)
+    {
+        Node *previous, *current;
+        if (!search(nim, &previous, &current))
+            return false;
+
+        if (current == START)
+        {
+            START = START->next;
+        }
+        else
+        {
+            previous->next = current->next;
+        }
+        delete current;
+        return true;
+    }
+
    
