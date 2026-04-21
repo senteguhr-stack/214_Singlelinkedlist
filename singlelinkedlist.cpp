e60f670af730253250df4d1bee82e83f0fca01ec
senteguhr <senteguhr@gmail.com>
2026-04-21 12:21:17 +0700

    struktur

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -1,4 +1,21 @@
 #include <iostream>
 #include <string>
 using namespace std;
+#include <iostream>
+#include <string>
+using namespace std;
 
+class Node
+{
+public:
+    int noMhs;
+    Node *next;
+};
+
+class LinkedList
+{
+    Node *START;
+
+public:
+    LinkedList()
+ 
